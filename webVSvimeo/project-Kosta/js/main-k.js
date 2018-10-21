13e786ca16da37c389592771a6993c9062593661
NikitaTarasenko <tarniki@rambler.ru>
2018-10-21 15:52:56 +0300

    4

--- webVSvimeo/project-Kosta/js/main-k.js
+++ webVSvimeo/project-Kosta/js/main-k.js
@@ -1,10 +1,10 @@
-if(!detectmob() || (!detectmob2()))
-	{
-		document.querySelector("#MyVideo").src = " styles/video/videoMain1-2.mp4";
+// if(!detectmob() || (!detectmob2()))
+// 	{
+// 		document.querySelector("#MyVideo").src = " styles/video/videoMain1-2.mp4";
 
-		// document.querySelector("#MyVideo").src = " styles/video/videoMain2-2.mp4";
-		// document.querySelector("#MyVideo").src = " styles/video/videoMain3-2.mp4";
-	}
+// 		// document.querySelector("#MyVideo").src = " styles/video/videoMain2-2.mp4";
+// 		// document.querySelector("#MyVideo").src = " styles/video/videoMain3-2.mp4";
+// 	}
 // window.onload = function ()
 // {
 // 		preloader = document.getElementById('page-preloader');
